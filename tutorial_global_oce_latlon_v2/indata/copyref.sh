3262f91ba32a739411b132fdae60668a706dd28d
hajsong <hajsong@gmail.com>
2024-04-11 09:07:46 +0900

    remove examples

--- tutorial_global_oce_latlon_v2/indata/copyref.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/csh -f
-
-cp -f ../run_ref/Eta.0000000020.data etan_obs.0000000020
-cp -f ../run_ref/Eta.0000000040.data etan_obs.0000000040
-cp -f ../run_ref/Eta.0000000060.data etan_obs.0000000060
-cp -f ../run_ref/Eta.0000000080.data etan_obs.0000000080
-cp -f ../run_ref/Eta.0000000100.data etan_obs.0000000100
-cp -f ../run_ref/Eta.0000000120.data etan_obs.0000000120
